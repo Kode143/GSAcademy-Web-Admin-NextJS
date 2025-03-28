e8a69a397dad49559278f126a8c9c010297ba80d
Bhuwan Shrestha <118792877+Kode143@users.noreply.github.com>
2025-03-28 06:19:10 +0100

    edit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,44 +1,43 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from '@/lib/mongodb';
-import { getServerSession } from "next-auth";
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from '@/lib/mongodb'
+import {getServerSession} from "next-auth";
 
-const adminEmails = ['sthabhuwan168@gmail.com'];
 
+const adminEmails= ['sthabhuwan168@gmail.com'];
 export const authOptions = {
   providers: [
+
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      // Optional: Uncomment this if you need to set a custom redirect URI
-      authorization: {
-        params: {
-          redirect_uri: 'https://admin.bhuwanshrestha143.com.np/api/auth/callback/google',
-        },
-      },
+      clientSecret: process.env.GOOGLE_SECRET
     }),
+ 
   ],
   secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter (clientPromise),
   callbacks: {
-    session: ({ session, token, user }) => {
-      if (adminEmails.includes(session?.user?.email)) {
+    session : ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)){
         return session;
-      } else {
+      } else{
         return false;
       }
+  
+      
     },
   },
-};
-
+}
 export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
-  const session = await getServerSession(req, res, authOptions);
+  const session = await getServerSession(req,res,authOptions);
 
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401).end();
-    throw new Error('not admin');
+  if(!adminEmails.includes(session?.user?.email)){
+    res.status(401);
+    res.end();
+    throw 'not admin';
+
   }
 }
